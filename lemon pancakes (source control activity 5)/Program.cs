ade6b040e5e0d0c929c35b2a725b84a9c3ba2219
Sneha Sil <snehasil413@gmail.com>
2021-06-13 15:06:48 -0500

    created project

---
+++ lemon pancakes (source control activity 5)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lemon_pancakes__source_control_activity_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
